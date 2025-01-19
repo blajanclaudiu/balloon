1ca5dddd1548cbdfdd78b655752cafcd33dcd7b1
blajanclaudiu <blajan.claudiu@yahoo.com>
2025-01-19 17:37:38 +0000

    Optimize UI - adding new feature

--- src/libs/transformers/models/qwen2_vl/image_processing_qwen2_vl.ts
+++ src/libs/transformers/models/qwen2_vl/image_processing_qwen2_vl.ts
@@ -3,12 +3,12 @@ import { cat, Tensor } from '../../utils/tensor.js';
 
 export class Qwen2VLImageProcessor extends ImageProcessor {
   async _call(images, ...args) {
-    var { pixel_values, original_sizes, reshaped_input_sizes } = await super._call(images, ...args);
+    const { pixel_values, original_sizes, reshaped_input_sizes } = await super._call(images, ...args);
 
     let patches = pixel_values;
 
     // @ts-ignore
-    var { temporal_patch_size, merge_size, patch_size } = this.config;
+    const { temporal_patch_size, merge_size, patch_size } = this.config;
     if (patches.dims[0] === 1) {
       // Equivalent to np.tile(patches, (self.temporal_patch_size, 1, 1, 1))
       patches = cat(
@@ -17,12 +17,12 @@ export class Qwen2VLImageProcessor extends ImageProcessor {
       );
     }
 
-    var grid_t = patches.dims[0] / temporal_patch_size;
-    var channel = patches.dims[1];
-    var grid_h = Math.floor(patches.dims[2] / patch_size);
-    var grid_w = Math.floor(patches.dims[3] / patch_size);
+    const grid_t = patches.dims[0] / temporal_patch_size;
+    const channel = patches.dims[1];
+    const grid_h = Math.floor(patches.dims[2] / patch_size);
+    const grid_w = Math.floor(patches.dims[3] / patch_size);
 
-    var flatten_patches = patches
+    const flatten_patches = patches
       .view(
         grid_t,
         temporal_patch_size,
@@ -37,7 +37,7 @@ export class Qwen2VLImageProcessor extends ImageProcessor {
       .permute(0, 3, 6, 4, 7, 2, 1, 5, 8)
       .view(grid_t * grid_h * grid_w, channel * temporal_patch_size * patch_size * patch_size);
 
-    var image_grid_thw = new Tensor('int64', [grid_t, grid_h, grid_w], [1, 3]);
+    const image_grid_thw = new Tensor('int64', [grid_t, grid_h, grid_w], [1, 3]);
 
     return {
       pixel_values: flatten_patches,
