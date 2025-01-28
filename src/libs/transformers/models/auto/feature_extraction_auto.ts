57350def40dca5cba1f004fd1b835ddd4d8be3cf
blajanclaudiu <blajan.claudiu@yahoo.com>
2025-01-28 14:40:17 +0000

    Improve README - removing deprecated function

--- src/libs/transformers/models/auto/feature_extraction_auto.ts
+++ src/libs/transformers/models/auto/feature_extraction_auto.ts
@@ -6,11 +6,11 @@ import * as AllFeatureExtractors from '../feature_extractors.js';
 export class AutoFeatureExtractor {
   /** @type {typeof FeatureExtractor.from_pretrained} */
   static async from_pretrained(pretrained_model_name_or_path: string, options = {}) {
-    let preprocessorConfig = await getModelJSON(pretrained_model_name_or_path, FEATURE_EXTRACTOR_NAME, true, options);
+    const preprocessorConfig = await getModelJSON(pretrained_model_name_or_path, FEATURE_EXTRACTOR_NAME, true, options);
 
     // Determine feature extractor class
-    let key = preprocessorConfig.feature_extractor_type;
-    let feature_extractor_class = AllFeatureExtractors[key as keyof typeof AllFeatureExtractors];
+    const key = preprocessorConfig.feature_extractor_type;
+    const feature_extractor_class = AllFeatureExtractors[key as keyof typeof AllFeatureExtractors];
 
     if (!feature_extractor_class) {
       throw new Error(`Unknown feature_extractor_type: '${key}'. Please report this at ${GITHUB_ISSUE_URL}.`);
