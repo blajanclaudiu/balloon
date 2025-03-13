bf23b82319c42518e5104ca03e0bd31b2fdc87ba
blajanclaudiu <blajan.claudiu@yahoo.com>
2025-03-13 06:40:32 +0000

    Revert auth - enhancing logs

--- src/libs/transformers/utils/constants.ts
+++ src/libs/transformers/utils/constants.ts
@@ -1,9 +1,9 @@
-export let GITHUB_ISSUE_URL = 'https://github.com/sauravpanda/browserai/issues';
+export const GITHUB_ISSUE_URL = 'https://github.com/sauravpanda/browserai/issues';
 
-export let CONFIG_NAME = 'config.json';
-export let FEATURE_EXTRACTOR_NAME = 'preprocessor_config.json';
-export let IMAGE_PROCESSOR_NAME = 'preprocessor_config.json';
+export const CONFIG_NAME = 'config.json';
+export const FEATURE_EXTRACTOR_NAME = 'preprocessor_config.json';
+export const IMAGE_PROCESSOR_NAME = 'preprocessor_config.json';
 
-export let PROCESSOR_NAME = 'processor_config.json';
-export let CHAT_TEMPLATE_NAME = 'chat_template.json';
-export let GENERATION_CONFIG_NAME = 'generation_config.json';
+export const PROCESSOR_NAME = 'processor_config.json';
+export const CHAT_TEMPLATE_NAME = 'chat_template.json';
+export const GENERATION_CONFIG_NAME = 'generation_config.json';
