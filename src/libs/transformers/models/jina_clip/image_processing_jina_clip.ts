abd39399ea0a9693f8fba93ac9ecee459760ceb9
blajanclaudiu <blajan.claudiu@yahoo.com>
2025-04-12 18:18:53 +0000

    Refactor UI - removing deprecated function

--- src/libs/transformers/models/jina_clip/image_processing_jina_clip.ts
+++ src/libs/transformers/models/jina_clip/image_processing_jina_clip.ts
@@ -3,16 +3,16 @@ import { ImageProcessor } from '../../base/image_processors_utils.js';
 export class JinaCLIPImageProcessor extends ImageProcessor {
   constructor(config) {
     // JinaCLIPImageProcessor uses a custom preprocessor_config.json, so we configure it here
-    let { resize_mode, fill_color, interpolation, size, ...other } = config;
+    const { resize_mode, fill_color, interpolation, size, ...other } = config;
 
-    let new_size =
+    const new_size =
       resize_mode === 'squash'
         ? { width: size, height: size }
         : resize_mode === 'shortest'
           ? { shortest_edge: size }
           : { longest_edge: size };
 
-    let resample = interpolation === 'bicubic' ? 3 : 2;
+    const resample = interpolation === 'bicubic' ? 3 : 2;
     super({
       ...other,
       size: new_size,
